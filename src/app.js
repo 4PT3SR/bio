6d9cfcdcc67477b30d07d7328a540ba46124d044
apt3sr <kingtoluene@gmail.com>
2022-10-30 17:51:39 +0100

    used get method and added route

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 9900;
 app.use(cors({origin:'*'}));
-app.use('*',(req,res,next)=> {
+app.get('/bio',(req,res,next)=> {
     try {
         const details = {
             slackUsername:"4pt3sr",
@@ -18,5 +18,8 @@ app.use('*',(req,res,next)=> {
         res.status(500).send({message:'Sorry,an error occured'});
     }
 })
+app.use('*',(req,res)=> {
+    res.status(404).send({message:'This endpoint does not exist'})
+});
 
 app.listen(PORT,()=> console.log(`Now listening on port ${PORT}`))
